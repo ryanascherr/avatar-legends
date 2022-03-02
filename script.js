54d50f61b2c8ce98844ff4d72c7d109cd3503b15
ryanascherr <ryanascherr@gmail.com>
2022-03-02 13:18:18 -0500

    Began working on stats page layout and functionality.

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 let playbook;
+let characters = [];
 
+localStorage.setItem("playbook", "");
+
+if (characters.length == 0) {
+    $(".playbooks").removeClass("hidden");
+    $(".your-characters").addClass("hidden");
+}
 
 $(".playbook-select").click(function() {
     playbook = $(this).data("playbook");
+
     localStorage.setItem("playbook", playbook);
+
+    displayPlaybook();
+})
+
+function displayPlaybook() {
+
+    if (playbook == "adamant") {
+        $(".feature-title").html("The Lodestar &#8595;");
+        $(".feature-content").html("<p>Adamant stuff</p>")
+    }
+
+    $(".playbooks").addClass("hidden");
+    $(".playbook-info").removeClass("hidden");
+}
+
+$(".feature-title").click(function() {
+    $(".feature-content").toggle();
 })
